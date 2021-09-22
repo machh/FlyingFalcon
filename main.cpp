8a5ccfb0a4d87a68e3659c64f52c1db8d4f16509
machh <haige521@126.com>
2021-09-22 20:21:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,8 +16,7 @@ std::string make_daytime_string() {
 }
 
 int main() {
-    try
-    {
+    try {
         asio::io_context io_context;
        
         tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8899));
@@ -30,9 +29,7 @@ int main() {
              asio::error_code ignored_error;
              asio::write(socket, asio::buffer(message), ignored_error);
         }
-     }
-     catch (std::exception& e)
-     {
+     } catch (std::exception& e) {
          std::cerr << e.what() << std::endl;
      }
 
